4c09222b2cd05d4440ac710d2f93662ccbe5390b
Kelvinsbs <nivlek.barcelos@gmail.com>
2022-02-17 21:44:12 -0300

    iniciando o modulo de usuarios

---
+++ usuarios/cadastro.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar Usuário</title>
+</head>
+<body>
+    <form action="cadastrar.php" method="post"></form>
+        <h1>Cadastrar Usuário</h1>
+        <div>
+            <label for="nome">Nome</label>
+            <input type="text" name="nome" id="nome">
+        </div>
+        <div>
+            <label for="login">Login</label>  
+            <input type="text" name="login" id="login">
+        </div>
+        <div>
+            <label for="senha">Senha</label>
+            <input type="password" name="senha" id="senha">
+        </div>
+        <div>
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email">
+        </div>
+    </form>
+
+    <script>
+        
+    </script>
+</body>
+</html>
